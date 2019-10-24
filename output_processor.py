fb60eb4ce9ffee0ff0d4d29f79b71fc5fca24fcc
sologuboved <sologuboved@gmail.com>
2019-10-25 00:04:25 +0300

    added verb output processor stub

---
+++ output_processor.py
@@ -0,0 +1,12 @@
+from global_vars import *
+from cooljugator_globals import *
+
+
+def process_verb_output(res, minimalistic):
+    pass
+
+
+def get_links(word):
+    return 'https://www.wordreference.com/gren/{}\n\n' \
+           'https://el.wiktionary.org/wiki/{}\n\n' \
+           'https://en.wiktionary.org/wiki/{}'.format(*[word] * 3)
