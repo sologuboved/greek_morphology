a5e36b481e36e1a7d73cb408d04514c8417e43f3
sologuboved <sologuboved@gmail.com>
2019-10-25 14:05:46 +0300

    fixed deprecated handlers

--- bot.py
+++ bot.py
@@ -5,23 +5,21 @@ from output_processor import get_links
 from tkn import TOKEN
 
 
-def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Get Gr-En reference")
+def start(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text="Get Gr-En reference")
 
 
-def send_minimalistic_verb(bot, update):
-    query = update['message']['text']
-    query = query.split()
-    process_verb_query(bot, update, query, minimalistic=True)
+def send_minimalistic_verb(update, context):
+    query = update['message']['text'].split()
+    process_verb_query(context.bot, update, query, minimalistic=True)
 
 
-def send_verb_paradigm(bot, update):
-    query = update['message']['text']
-    query = query.split()
-    process_verb_query(bot, update, query, minimalistic=False)
+def send_verb_paradigm(update, context):
+    query = update['message']['text'].split()
+    process_verb_query(context.bot, update, query, minimalistic=False)
 
 
-def send_links(bot, update):
+def send_links(update, context):
     word = update.message.text
     update.message.reply_text(get_links(word))
 
@@ -29,13 +27,17 @@ def send_links(bot, update):
 def main():
     updater = Updater(token=TOKEN, use_context=True)
     dispatcher = updater.dispatcher
+
     start_handler = CommandHandler('start', start)
     verb_handler = CommandHandler('v', send_minimalistic_verb)
     conjug_handler = CommandHandler('conjug', send_verb_paradigm)
+
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(verb_handler)
     dispatcher.add_handler(conjug_handler)
+
     dispatcher.add_handler(MessageHandler(filters=Filters.text, callback=send_links))
+
     updater.start_polling()
     updater.idle()
 
