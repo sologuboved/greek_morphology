79e0e2567b723e5b3d4b60c3c90454f27ee9526b
sologuboved <sologuboved@gmail.com>
2019-11-05 16:26:35 +0300

    '' --> None

--- cooljugator_processor.py
+++ cooljugator_processor.py
@@ -15,10 +15,8 @@ def process_paradigms(final_paradigm_json, raw_paradigm_json=RAW_COOLJUGATOR_PAR
 def process_line(line):
     return {VERB: line[VERB], TRANSL: line[TRANSL],
             FUTURUM: line[FUTURE1], AORIST: line[PASTPERFECT1],
-            PARADIGM: [line[fieldname] for fieldname in COOLJUGATOR_FIELDNAMES]}
+            PARADIGM: [line[fieldname] if line[fieldname] else None for fieldname in COOLJUGATOR_FIELDNAMES]}
 
 
 if __name__ == '__main__':
     process_paradigms(COOLJUGATOR_PARADIGM_JSON)
-
-
