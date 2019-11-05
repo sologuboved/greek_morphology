565bed3591f3e4a5790d1d30398df261e42b7fa0
sologuboved <sologuboved@gmail.com>
2019-11-05 16:42:39 +0300

    θα'ed output

--- output_processor.py
+++ output_processor.py
@@ -1,14 +1,18 @@
 from global_vars import NOT_FOUND, MISSING_WORDS_TXT
-from cooljugator_globals import COOLJUGATOR_SLICES
+from cooljugator_globals import COOLJUGATOR_SLICES, FUTURE
 
 
 def get_verb(res, minimalistic):
     if minimalistic:
         return "{} - {} - {}\n<i>{}</i>".format(*res)
-    return "\n\n".join(
-        ["<b>{}:</b>\n{}".format(label,
-                                 ", ".join(filter(lambda x: x, res[tense]))) for label, tense in COOLJUGATOR_SLICES]
-    )
+    paradigm = list()
+    for label, tense in COOLJUGATOR_SLICES:
+        if label == FUTURE:
+            forms = map(lambda x: "θα " + x, filter(lambda x: x, res[tense]))
+        else:
+            forms = filter(lambda x: x, res[tense])
+        paradigm.append("<b>{}:</b>\n{}".format(label, ", ".join(forms)))
+    return "\n\n".join(paradigm)
 
 
 def get_missing_words(query):
