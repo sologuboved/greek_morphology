9a3e3853e42f5191d466e0bb4f3e02677918233e
sologuboved <sologuboved@gmail.com>
2019-11-06 21:36:50 +0300

    accounted for array-typed fieldcontents

--- output_processor.py
+++ output_processor.py
@@ -1,18 +1,35 @@
-from global_vars import NOT_FOUND, MISSING_WORDS_TXT
+from global_vars import NOT_FOUND, MISSING_WORDS_TXT, VERB, AORIST, FUTURUM, TRANSL, PARADIGM
 from cooljugator_globals import COOLJUGATOR_SLICES, FUTURE
 
 
 def get_verb(res, minimalistic):
+    def process_sequence(raw_seq):
+        seq = list()
+        for item in raw_seq:
+            if isinstance(item, list):
+                seq.append(" / ".join(item))
+            else:
+                seq.append(item)
+        return seq
+
+    def get_minimalistic():
+        return "{} - {} - {}\n<i>{}</i>".format(*process_sequence([res[VERB], res[AORIST], res[FUTURUM], res[TRANSL]]))
+
+    def get_tense():
+        return process_sequence(filter(lambda x: x, res[tense]))
+
     if minimalistic:
-        return "{} - {} - {}\n<i>{}</i>".format(*res)
+        return get_minimalistic()
+
+    res = res[PARADIGM]
     if not res:
-        return "Paradigm unknown"
+        return "Paradigm is missing\n\n{}".format(get_minimalistic())
     paradigm = list()
     for label, tense in COOLJUGATOR_SLICES:
         if label == FUTURE:
-            forms = map(lambda x: "θα " + x, filter(lambda x: x, res[tense]))
+            forms = map(lambda x: "θα " + x, get_tense())
         else:
-            forms = filter(lambda x: x, res[tense])
+            forms = get_tense()
         paradigm.append("<b>{}:</b>\n{}".format(label, ", ".join(forms)))
     return "\n\n".join(paradigm)
 
