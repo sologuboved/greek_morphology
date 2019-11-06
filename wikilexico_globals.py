3dc3e17de8f1035e106819bdb5a071f818778af0
sologuboved <sologuboved@gmail.com>
2019-11-06 21:02:46 +0300

    removed redundant variable

--- wikilexico_globals.py
+++ wikilexico_globals.py
@@ -1,4 +1,3 @@
 UNFILTERED_WIKILEXICO_LIST_JSON = 'wikilexico_list_unfiltered.json'
 FILTERED_WIKILEXICO_LIST_JSON = 'wikilexico_list.json'
-RAW_WIKILEXICO_PARADIGM_JSON = 'wikilexico_paradigm_raw.json'
 WIKILEXICO_PARADIGM_JSON = 'wikilexico_paradigm.json'
