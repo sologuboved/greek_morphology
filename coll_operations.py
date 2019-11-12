dffe9eb9446404de7044c7a05739098a3fd26bee
sologuboved <sologuboved@gmail.com>
2019-11-12 21:35:44 +0300

    more operations

--- coll_operations.py
+++ coll_operations.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient, ASCENDING
-from global_vars import DB_NAME, VERBS, LOCALHOST, PORT, VERB, PARADIGM
+from global_vars import *
 from helpers import counter
 
 
@@ -54,6 +54,36 @@ def add_indices(target, indices):
             target.create_index([(indx, ASCENDING)])
 
 
+def print_verbs(fieldname, fltr, func=None, dbname=DB_NAME, collname=VERBS):
+    match = MongoClient(LOCALHOST, PORT)[dbname][collname].find(fltr)
+    total = match.count()
+    if func:
+        res = list()
+        count = counter(total)
+        for entry in match:
+            next(count)
+            if func(entry):
+                res.append(entry[fieldname])
+        print("\n{} matching items".format(len(res)))
+    else:
+        print(total, "matching entries")
+        res = [entry[fieldname] for entry in match]
+    for item in res:
+        print(item)
+
+
+def stringify(fieldcontent):
+    if isinstance(fieldcontent, str):
+        return fieldcontent
+    else:
+        return " / ".join(fieldcontent)
+
+
+def is_abnormal_verb(entry):
+    verb = stringify(entry[VERB])
+    return not (verb.endswith('ω') or verb.endswith('ώ') or verb.endswith('αι'))
+
+
 if __name__ == '__main__':
     # from global_vars import SOURCE, WIKILEXICO_ACT_VERBS
     # add_field(SOURCE, 'w', collname=WIKILEXICO_ACT_VERBS)
@@ -62,4 +92,5 @@ if __name__ == '__main__':
     # copy_collection(target_collname=VERBS + '_backup')
     # from global_vars import TRANSL, WIKILEXICO_ACT_VERBS
     # edit_field(TRANSL, lambda x: str(), collname=WIKILEXICO_ACT_VERBS)
+    print_verbs(VERB, {SOURCE: 'c'}, is_abnormal_verb)
     ...
