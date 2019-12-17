03087613b98e307f00e7a67b8fb09f40d70f423b
sologuboved <sologuboved@gmail.com>
2019-12-17 21:59:38 +0300

    generalized rows & paradigm getters

--- wikilexico_scraper.py
+++ wikilexico_scraper.py
@@ -11,6 +11,7 @@ from helpers import which_watch, counter, write_json_lines, read_json_lines
 @which_watch
 @write_json_lines
 def collect_verbs(unfiltered_list_json):
+    print("Collecting verbs to", unfiltered_list_json)
     page_url = 'https://el.wiktionary.org/w/index.php?title=%CE%9A%CE%B1%CF%84%CE%B7%CE%B3%CE%BF%CF%81%CE%AF%CE%B1:' \
                '%CE%A1%CE%AE%CE%BC%CE%B1%CF%84%CE%B1_(%CE%BD%CE%AD%CE%B1_%CE%B5%CE%BB%CE%BB%CE%B7%CE%BD%CE%B9%CE%BA' \
                '%CE%AC)&from=%CE%B1#mw-pages'
@@ -26,17 +27,20 @@ def collect_verbs(unfiltered_list_json):
             page_url = 'https://el.wiktionary.org' + soup.find('a', href=True, text="επόμενη σελίδα")['href']
         except TypeError:
             break
+    print()
 
 
 @which_watch
 @write_json_lines
 def filter_verbs(filtered_list_json, unfiltered_list_json=UNFILTERED_WIKILEXICO_LIST_JSON):
+    print("Filtering {} --> {}".format(unfiltered_list_json, filtered_list_json))
     count = counter()
     coll = MongoClient(LOCALHOST, PORT)[DB_NAME][VERBS]
     for verb in read_json_lines(unfiltered_list_json):
         next(count)
         if not coll.find({VERB: verb}).count():
             yield verb
+    print()
 
 
 @which_watch
@@ -46,7 +50,7 @@ def collect_active_voice_paradigms(raw_paradigm_json, list_json=FILTERED_WIKILEX
     for verb in read_json_lines(list_json):
         next(count)
         try:
-            paradigm = get_active_voice_paradigm(verb)
+            paradigm = get_paradigm(verb, active_voice=True)
         except Exception as e:
             print()
             print(verb)
@@ -58,7 +62,7 @@ def collect_active_voice_paradigms(raw_paradigm_json, list_json=FILTERED_WIKILEX
             yield entry
 
 
-def get_active_voice_paradigm(verb):
+def get_paradigm(verb, active_voice):
 
     def get_columns():
         return rows[index].find_all('td')
@@ -83,7 +87,7 @@ def get_active_voice_paradigm(verb):
     imperfect = list()
     imperative = list()
 
-    rows = get_active_voice_rows(verb)
+    rows = get_rows(verb, active_voice)
     if not rows:
         return
 
@@ -121,17 +125,35 @@ def get_active_voice_paradigm(verb):
                                 list(filter(lambda v: v, imperative))))
 
 
-def get_active_voice_rows(verb):
+# def get_active_voice_rows(verb):
+#     verb = BeautifulSoup(requests.get('https://el.wiktionary.org/wiki/' + verb).content, 'lxml')
+#     if not verb.find('span', {'class': 'inflexions'}):
+#         return
+#     for table in verb.find_all('tbody'):
+#         try:
+#             first_header = table.find('center').text.strip()
+#         except AttributeError:
+#             continue
+#         if first_header == "Εξακολουθητικοί χρόνοι":
+#             return table.find_all('tr')
+
+
+def get_rows(verb, active_voice):
     verb = BeautifulSoup(requests.get('https://el.wiktionary.org/wiki/' + verb).content, 'lxml')
     if not verb.find('span', {'class': 'inflexions'}):
         return
+    omitted_active = False
     for table in verb.find_all('tbody'):
         try:
             first_header = table.find('center').text.strip()
         except AttributeError:
             continue
         if first_header == "Εξακολουθητικοί χρόνοι":
-            return table.find_all('tr')
+            if omitted_active or active_voice:
+                return table.find_all('tr')
+            else:
+                omitted_active = True
+                continue
 
 
 def fix_future(raw_future):
@@ -151,6 +173,7 @@ def get_shortened(paradigm):
 if __name__ == '__main__':
     # collect_verbs(UNFILTERED_WIKILEXICO_LIST_JSON)
     # filter_verbs(FILTERED_WIKILEXICO_LIST_JSON)
-    collect_active_voice_paradigms(WIKILEXICO_PARADIGM_JSON)
+    # collect_active_voice_paradigms(WIKILEXICO_PARADIGM_JSON)
     # print(get_active_voice_paradigm('αγανοϋφαίνω'))
     # get_active_voice_rows('αγανοϋφαίνω')
+    print(get_paradigm('αερίζω', False))
