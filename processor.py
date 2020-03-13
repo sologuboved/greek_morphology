fe4d30d3809888ca8cbd7aeedaea49ee0f8c8990
sologuboved <sologuboved@gmail.com>
2020-03-13 20:29:00 +0300

    changed formatting

--- processor.py
+++ processor.py
@@ -61,12 +61,13 @@ def process_links_query(update):
     else:
         paradigm = get_verb(paradigm, minimalistic=True, appendix=True)
     update.message.reply_text(
+        '<a href="https://translate.google.com/#view=home&op=translate&sl=el&tl=en&text={encoded_word}">'
+        'Google Translate</a>\n\n'
+        '<a href="https://www.multitran.com/m.exe?l1=1&l2=38&s={word}">Multitran</a>\n\n'
+        '<a href="https://el.wiktionary.org/wiki/{word}">Βικιλεξικό</a>\n\n'
+        '<a href="https://en.wiktionary.org/wiki/{word}">Wiktionary</a>\n\n'
         'https://www.wordreference.com/gren/{word}\n\n'
-        'https://el.wiktionary.org/wiki/{word}\n\n'
-        'https://en.wiktionary.org/wiki/{word}\n\n'
-        'https://www.lexigram.gr/lex/newg/{word}\n\n'
-        'https://translate.google.com/#view=home&op=translate&sl=el&tl=en&text={encoded_word}\n\n'
-        'https://www.multitran.com/m.exe?l1=1&l2=38&s={word}'
+        'https://www.lexigram.gr/lex/newg/{word}\n'
         '{fem_nom_pl}'
         '{paradigm}'.format(word=word, encoded_word=quote(word), fem_nom_pl=fem_nom_pl, paradigm=paradigm),
         disable_web_page_preview=True, parse_mode=ParseMode.HTML
